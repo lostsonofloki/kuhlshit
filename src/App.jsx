445b11c392c3157cc00fd1005dd55e40b04c2611
losts <sonofloke@gmail.com>
2026-04-08 10:52:42 -0500

    Add Vercel Analytics, Google Analytics tag, Munson & Brothers links

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/next'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import AnnouncementBar from './components/AnnouncementBar'
@@ -32,6 +33,7 @@ function App() {
         </div>
       </main>
       <Footer />
+      <Analytics />
     </div>
   )
 }
